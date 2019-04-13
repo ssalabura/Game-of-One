6dbbb7c5d12d92cc0e1e7713a704a99378134b75
ssalabura <stagerek@gmail.com>
2019-04-13 15:24:13 +0200

    JavaFX Hello World

--- src/main/java/org/bitbucket/GameofOneTeam/gameofone/App.java
+++ src/main/java/org/bitbucket/GameofOneTeam/gameofone/App.java
@@ -1,13 +1,35 @@
 package org.bitbucket.GameofOneTeam.gameofone;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    Button btn = new Button();
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Game of One");
+        btn.setText("Hello World!");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            public void handle(ActionEvent event) {
+                App.this.btn.setText("Clicked the button");
+            }
+        });
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
     }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
 }
