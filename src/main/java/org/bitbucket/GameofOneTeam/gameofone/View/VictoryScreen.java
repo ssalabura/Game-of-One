26daea51c5676cfffb05be2873f0c8592b4fd0d4
z1154450 <d.barczyk99@gmail.com>
2019-04-29 01:02:57 +0200

    Functional victory screen

--- src/main/java/org/bitbucket/GameofOneTeam/gameofone/View/VictoryScreen.java
+++ src/main/java/org/bitbucket/GameofOneTeam/gameofone/View/VictoryScreen.java
@@ -14,15 +14,18 @@ import org.bitbucket.GameofOneTeam.gameofone.Controller.GameController;
 import org.bitbucket.GameofOneTeam.gameofone.Model.ClassicGameModel;
 
 public class VictoryScreen extends Scene {
-    private final static Font btnStyle = Font.font("Ubuntu Mono",50);
-    private final static StackPane victoryRoot = new StackPane();
-    private final static VBox vb = new VBox(50);
-    private final static VBox button = new VBox(20);
-    private final static Button backToMenuButton = new Button();
-    private final static Text title = new Text("Game Finished!");
+    private static Font btnStyle = Font.font("Ubuntu Mono",50);
+    private static StackPane victoryRoot = new StackPane();
+    private static VBox vb = new VBox(50);
+    private static VBox button = new VBox(20);
+    private static Button backToMenuButton = new Button();
+    private static Text title = new Text("Game Finished!");
     private static Text winner = new Text();
     private static int winnerNum = 0;
-    static {
+    public VictoryScreen(int w, int h) {
+        super(victoryRoot, w, h);
+    }
+    public void setWinner(int i){
         title.setFont(Font.font("Open Sans",150));
         title.setStyle("-fx-fill: white;" +
                 "-fx-stroke: black;" +
@@ -42,12 +45,10 @@ public class VictoryScreen extends Scene {
         vb.setAlignment(Pos.CENTER);
         victoryRoot.getChildren().add(vb);
         victoryRoot.setBackground(new Background(new BackgroundImage(View.background, BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT)));
-    }
-    public VictoryScreen(int w, int h) {
-        super(victoryRoot, w, h);
-    }
-    public void setWinner(int i){
+        victoryRoot.getChildren().clear();
         winnerNum = i;
         if(winnerNum==0) winner = new Text("Congratulations, you win!");
-        else winner = new Text("Player "+winnerNum+" wins! You lose!");}
+        else winner = new Text("Player "+winnerNum+" wins! You lose!");
+
+    }
 }
