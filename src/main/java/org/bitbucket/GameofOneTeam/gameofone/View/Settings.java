9789ef14f6c5792a59358c083c30c01894e6fb19
ssalabura <stagerek@gmail.com>
2019-05-21 21:26:39 +0200

    fix

--- src/main/java/org/bitbucket/GameofOneTeam/gameofone/View/Settings.java
+++ src/main/java/org/bitbucket/GameofOneTeam/gameofone/View/Settings.java
@@ -5,7 +5,6 @@ import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Slider;
 import javafx.scene.image.Image;
 import javafx.scene.layout.*;
 import javafx.scene.media.AudioClip;
